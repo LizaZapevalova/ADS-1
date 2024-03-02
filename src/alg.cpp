f43ff924cb305dad445c286196ba40529c89a194
LizaZapevalova <160137500+LizaZapevalova@users.noreply.github.com>
2024-03-02 15:14:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,23 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (num <= 1){
+      return false;
+  }
+
+  for (int i = 2; i * i <= num; i++){
+      if (num % i == 0){
+        return false;
+      }
+  }
+
+  return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-// вставьте код функции
-}
-
-uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
-}
-
-uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+int checkPrime(uint64_t value) {
+  while (true){
+    num++;
+    if (isPrime(num)){
+      return num;
+    }
 }
