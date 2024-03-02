6e9fb0c690b1083625a63fbcd4db2f6092df8959
LizaZapevalova <160137500+LizaZapevalova@users.noreply.github.com>
2024-03-02 18:47:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,35 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-bool checkPrime(uint64_t value){
+bool checkPrime(uint64_t num){
   if (num <= 1){
     return false;
   }
-  uint64_t nPrime(uint64_t n){
+
     for (int i = 2; i * i <= num; i++){
       if (num % i == 0){
         return false;
       }
     }
-    uint64_t nextPrime(uint64_t value){
       return true;
+    uint64_t nextPrime(uint64_t n){
+      if (n <= 0){
+        return 0;
     }
-     uint64_t sumPrime(uint64_t hbound){
-       while (true){
-         num++;
-         if (isPrime(num)){
-           return num;
-         }
-       }
+uint64_t count = 0;
+  uint64_t num = 2;
+    while (count < n){
+     if (isPrime(num)){
+       count++;
      }
+     num++;
+   }
+int main(){
+uint64_t n;
+std::cout << 'Введите номер простого числа';
+std::cin >> n;
+uint64_t result = nPrime(n);
+std::cout << 'Простое число под номером ' << n << 'равно ' << result << std::endl;
+return 0;
+}
+ 
