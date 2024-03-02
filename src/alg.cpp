79c90e4bc8705d4a19fea4d7387817f6af0246fa
LizaZapevalova <160137500+LizaZapevalova@users.noreply.github.com>
2024-03-02 14:43:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,7 @@
-// Copyright 2022 NNTU-CS
+   - bool checkPrime(uint64_t value) - проверка числа на простоту.
+   - uint64_t nPrime(uint64_t n) - нахождение n-ого простого числа (в ряду).
+   - uint64_t nextPrime(uint64_t value) - нахождение ближайшего следующего простого числа к value.
+   - uint64_t sumPrime(uint64_t hbound) - сумма всех чисел до hbound (не включая его)// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
